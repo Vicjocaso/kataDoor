022e42eb21e19006789a9452a49217cbc84ae939
victor Jose Calderon <victorjosecaderon@victors-MacBook-Air-2.local>
2021-12-05 19:41:04 -0400

    templ

---
+++ main.js
@@ -0,0 +1,41 @@
+class Door {
+  constructor(opened, locked, key) {
+    this.opened = opened;
+    this.locked = locked;
+    this.key = key;
+  }
+}
+const arr = () => "test";
+
+// Door: Open, Closed, Locked, Unlocked
+//     Open -> Closed
+//     Closed -> Open
+//     Unlocked -> Locked
+//     Locked -> Unlocked
+
+//     Open -> Open : error
+//     Closed -> Closed : error
+//     ...
+
+// --------------------------------------------
+// Door.OpenAndUnlocked(key: guid)
+// Door.OpenAndLocked(key: guid)
+// Door.ClosedAndUnlocked(key: guid)
+// Door.ClosedAndLocked(key: guid)
+
+// val door = Door.xyz
+
+// door.Open() //when Locked error
+// door.Close()
+// door.Lock()
+// door.Unlock(key)
+
+// door.IsOpen, IsClosed, IsLocked, IsUnlocked -> boolean
+
+// Room 1: OOP, Imperative, Mutable
+//     val doorA = Door.OpenAndLocked(someKey); // Open + Locked
+//     doorA.Close(); // Closed + Locked
+
+// Room 2: OOP, Functional (no mutation)
+//     val doorA = Door.OpenAndLocked(someKey); // Open + Locked
+//     val doorB = doorA.Close(); // Closed + Locked
---
+++ test.js
@@ -0,0 +1,6 @@
+describe("Door creation methods", () => {
+  it("Verify correct creation of a closed and unlocked door with a key 'key1'", () => {
+    let test = arr();
+    assert.equal(test, "test");
+  });
+});
